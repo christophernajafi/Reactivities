aa789042697b90d43feea8e62f393036d499c868
Christopher Najafi <chris.najafi@gmail.com>
2020-12-22 12:06:44 -0500

    Added components for getting activities from API; added ability to add, delete, and update activities on client.

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,55 @@
+import React, { Fragment } from "react";
+import { Button, Card, Image } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+
+interface IProps {
+  activity: IActivity;
+  setEditMode: (editMode: boolean) => void;
+  setSelectedActivity: (activity: IActivity | null) => void;
+}
+
+const ActivityDetails: React.FC<IProps> = ({
+  activity,
+  setEditMode,
+  setSelectedActivity,
+}) => {
+  // console.log("Activity: ", activity);
+  if (activity) {
+    return (
+      <Card fluid>
+        <Image
+          src={`/assets/categoryImages/${activity.category}.jpg`}
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          <Card.Header>{activity.title}</Card.Header>
+          <Card.Meta>
+            <span>{activity.date}</span>
+          </Card.Meta>
+          <Card.Description>{activity.description}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Button.Group widths={2}>
+            <Button
+              onClick={() => setEditMode(true)}
+              basic
+              color="blue"
+              content="Edit"
+            />
+            <Button
+              onClick={() => setSelectedActivity(null)}
+              basic
+              color="grey"
+              content="Cancel"
+            />
+          </Button.Group>
+        </Card.Content>
+      </Card>
+    );
+  } else {
+    return <Fragment />;
+  }
+};
+
+export default ActivityDetails;
