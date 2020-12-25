355baae1aa0db9a27022eb6ce2092f0b27c719c4
Christopher Najafi <chris.najafi@gmail.com>
2020-12-24 19:23:18 -0500

    Refactored client-app to use React Router.

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,51 +1,61 @@
 import { observer } from "mobx-react-lite";
-import React, { Fragment, useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import { Link, RouteComponentProps } from "react-router-dom";
 import { Button, Card, Image } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 import ActivityStore from "../../../app/stores/activityStore";
 
-const ActivityDetails: React.FC = () => {
+interface DetailParams {
+  id: string;
+}
+
+const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
+  match,
+  history,
+}) => {
   const activityStore = useContext(ActivityStore);
-  const {
-    selectedActivity: activity,
-    openEditForm,
-    cancelSelectedActivity,
-  } = activityStore;
-  if (activity) {
-    return (
-      <Card fluid>
-        <Image
-          src={`/assets/categoryImages/${activity.category}.jpg`}
-          wrapped
-          ui={false}
-        />
-        <Card.Content>
-          <Card.Header>{activity.title}</Card.Header>
-          <Card.Meta>
-            <span>{activity.date}</span>
-          </Card.Meta>
-          <Card.Description>{activity.description}</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <Button.Group widths={2}>
-            <Button
-              onClick={() => openEditForm(activity.id)}
-              basic
-              color="blue"
-              content="Edit"
-            />
-            <Button
-              onClick={cancelSelectedActivity}
-              basic
-              color="grey"
-              content="Cancel"
-            />
-          </Button.Group>
-        </Card.Content>
-      </Card>
-    );
-  } else {
-    return <Fragment />;
-  }
+  const { activity, loadActivity, loadingInitial } = activityStore;
+
+  useEffect(() => {
+    loadActivity(match.params.id);
+  }, [loadActivity, match.params.id]);
+
+  if (loadingInitial || !activity)
+    return <LoadingComponent content="Loading Activity" />;
+
+  return (
+    <Card fluid>
+      <Image
+        src={`/assets/categoryImages/${activity.category}.jpg`}
+        wrapped
+        ui={false}
+      />
+      <Card.Content>
+        <Card.Header>{activity.title}</Card.Header>
+        <Card.Meta>
+          <span>{activity.date}</span>
+        </Card.Meta>
+        <Card.Description>{activity.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group widths={2}>
+          <Button
+            as={Link}
+            to={`/manage/${activity.id}`}
+            basic
+            color="blue"
+            content="Edit"
+          />
+          <Button
+            onClick={() => history.push("/activities")}
+            basic
+            color="grey"
+            content="Cancel"
+          />
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  );
 };
 
 export default observer(ActivityDetails);
