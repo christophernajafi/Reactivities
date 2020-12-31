b667d3d19b282c25ad9a9da914d958a1d4ada3b5
Christopher Najafi <chris.najafi@gmail.com>
2020-12-31 10:41:10 -0500

    Added profile photo upload, set main, and delete feature to API.

--- Application/User/Register.cs
+++ Application/User/Register.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Identity;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
 using Application.Validators;
+using System.Linq;
 
 namespace Application.User
 {
@@ -73,7 +74,7 @@ namespace Application.User
                         DisplayName = user.DisplayName,
                         Token = _jwtGenerator.CreateToken(user),
                         Username = user.UserName,
-                        Image = null
+                        Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
                     };
                 }
 
